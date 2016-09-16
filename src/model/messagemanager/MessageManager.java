d5fb0180e06b5e63132ac89c0ce51be4a781f081
athompson909 <athompson909@gmail.com>
2016-09-16 10:41:22 -0600

    adam, adding empty map functions...

--- src/model/messagemanager/MessageManager.java
+++ src/model/messagemanager/MessageManager.java
@@ -1,7 +1,33 @@
 package model.messagemanager;
 
+import java.awt.*;
+
 /**
  * Created by Mitchell on 9/15/2016.
  */
 public class MessageManager {
+
+    private String messageLine;
+
+    private String source;//the player's username
+
+    public Color getUserColor() {
+        return Color.BLACK;
+    }
+
+    public String getMessageLine() {
+        return messageLine;
+    }
+
+    public void setMessageLine(String messageLine) {
+        this.messageLine = messageLine;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
 }
