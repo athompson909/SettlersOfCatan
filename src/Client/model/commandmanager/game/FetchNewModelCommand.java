734d54b02a19ef4aab1d70388d001a7b6c1ff689
wiseman5 <wiseman5@studentbody.byu.edu>
2016-09-19 10:09:57 -0600

    Command - game 1st half

--- src/Client/model/commandmanager/game/FetchNewModelCommand.java
+++ src/Client/model/commandmanager/game/FetchNewModelCommand.java
@@ -6,7 +6,25 @@ import Client.model.commandmanager.BaseCommand;
  * Created by Alise on 9/18/2016.
  */
 public class FetchNewModelCommand extends BaseCommand {
-    public FetchNewModelCommand(){
+    /**
+     * version of the client model
+     */
+    private int version;
+
+    /**
+     * Creates FetchNewModelCommand to send to ClientFacade
+     * Sets data members
+     * @param version = client model version number
+     */
+    public FetchNewModelCommand(int version){
+
+    }
+
+    /**
+     * Asks server if there is an updated model
+     */
+    @Override
+    public void serverExec(BaseCommand command){
 
     }
 }
--- src/Client/model/commandmanager/game/GameLoadCommand.java
+++ src/Client/model/commandmanager/game/GameLoadCommand.java
@@ -6,7 +6,25 @@ import java.io.File;
  * Created by Alise on 9/18/2016.
  */
 public class GameLoadCommand extends BaseCommand {
+    /**
+     * name of file to Load
+     */
+    private File filename;
+
+    /**
+     * Creates GameLoadCommand to send to the ClientFacade. Sets fileName.
+     * @param fileName
+     */
     public GameLoadCommand(File fileName){
+
+    }
+
+    /**
+     * Tells server to load given file
+     * @param command
+     */
+    @Override
+    public void serverExec(BaseCommand command){
 
     }
 }
