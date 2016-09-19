a9eb1678cbff781cd40d21ee512dba2345e42285
wiseman5 <wiseman5@studentbody.byu.edu>
2016-09-18 22:46:14 -0600

    IServerProxy and some commands

--- src/Client/model/commandmanager/BaseCommand.java
+++ src/Client/model/commandmanager/BaseCommand.java
@@ -4,4 +4,14 @@ package Client.model.commandmanager;
  * Created by Mitchell on 9/15/2016.
  */
 public class BaseCommand {
+    /**
+     *
+     */
+    public void clientExec(){
+
+    }
+
+    public void serverExec() {
+
+    }
 }
---
+++ src/Client/model/commandmanager/LoginCommand.java
@@ -0,0 +1,10 @@
+package Client.model.commandmanager;
+
+/**
+ * Created by Alise on 9/18/2016.
+ */
+public class LoginCommand extends BaseCommand {
+    LoginCommand(String username, String password){
+
+    }
+}
