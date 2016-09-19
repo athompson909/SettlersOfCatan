d3f381641ae3d21a80882e76251e9d69f6d12de6
sierrad012 <sd.birdlover@gmail.com>
2016-09-18 22:48:53 -0600

    Added stub classes for UpdateManager, DiceManager, and TradeOffer

---
+++ src/server/ServerPoller.java
@@ -0,0 +1,10 @@
+package server;
+
+/**
+ * Created by Mitchell on 9/15/2016.
+ * Sierra renamed this class from ServerPuller to ServerPoller on 9/18/2016 :)
+ */
+public class ServerPoller {
+
+
+}
