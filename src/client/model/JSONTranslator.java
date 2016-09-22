6351a9830052996f4117d212e1d6375938e4135e
sierrad012 <sd.birdlover@gmail.com>
2016-09-22 08:47:25 -0600

    JSONTranslator

---
+++ src/client/model/JSONTranslator.java
@@ -0,0 +1,28 @@
+package client.model;
+
+import com.google.gson.*;
+import org.json.*;
+
+/**
+ * JSONTranslator gets the new model from the server as a huge string, converts it to a JSONObject,
+ * uses GSON to break it down into objects, then sends those new objects to the
+ * ClientUpdateManager to be distributed to the existing ClientModel objects.
+ *
+ *
+ * Created by Sierra on 9/22/16.
+ */
+public class JSONTranslator {
+
+    /**
+     * Constructor
+     */
+    public void JSONTranslator(){
+
+    }
+
+
+
+
+
+
+}
