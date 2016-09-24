4882a2353637fb3d6130842f57a05c7237bb241b
wiseman5 <wiseman5@studentbody.byu.edu>
2016-09-24 11:27:29 -0600

    dice

---
+++ src/tests/client_tests/DiceManagerTest.java
@@ -0,0 +1,17 @@
+package tests.client_tests;
+
+import junit.framework.TestCase;
+import shared.model.dicemanager.DiceManager;
+
+/**
+ * Created by Alise on 9/24/2016.
+ */
+class DiceManagerTest extends TestCase {
+    private DiceManager manager = new DiceManager();
+    public void testRollDice() {
+        int number = manager.rollDice();
+        assert(number >= 1);
+        assert(number <= 12);
+        System.out.print(number);
+    }
+}
