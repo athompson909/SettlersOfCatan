8be1560bfddf16e1e6480b53d217f7b85e53be95
SierraD012 <sd.birdlover@gmail.com>
2016-09-26 09:36:44 -0600

    JSONTranslatorTest - filling out cmd translator functions

--- src/tests/client_tests/JSONTranslatorTest.java
+++ src/tests/client_tests/JSONTranslatorTest.java
@@ -1,5 +1,6 @@
 package tests.client_tests;
 
+import org.hamcrest.*;
 import org.json.JSONObject;
 import shared.definitions.CatanColor;
 import shared.locations.EdgeDirection;
@@ -12,12 +13,13 @@ import com.google.gson.*;
 import shared.model.JSONTranslator;
 import shared.model.commandmanager.game.*;
 import shared.model.commandmanager.moves.*;
+import shared.model.map.BuildCity;
 
 
 /**
  * This is the JUnit test for the JSONTranslator class.
  * I'm mainly using it to experiment with/understand Gson.
- *
+ * <p>
  * Created by Sierra on 9/23/16.
  */
 public class JSONTranslatorTest extends TestCase {
@@ -25,14 +27,48 @@ public class JSONTranslatorTest extends TestCase {
     private JSONTranslator jsonTranslator = new JSONTranslator();
     private String testResponseBody = null;
     private Gson gsonTest = new Gson();
+    private AddAICommand addAICommand;
+    private ExecuteGameCommandsCommand execGameCmdsCommand;
+    private FetchNewModelCommand fetchNewModelCommand;
+    private GameCreateCommand gameCreateCommand;
+    private GameListCommand gameListCommand;
+    private GameLoadCommand gameLoadCommand;
+    private GameResetCommand gameResetCommand;
+    private GameSaveCommand gameSaveCommand;
+    private GetGameCommandsCommand getGameCmdsCommand;
+    private ListAICommand listAICommand;
+    private LoginCommand loginCommand;
+    private RegisterCommand registerCommand;
+    private SendChatCommand sendChatCommand;
+    private UtilChangeLogLevelCommand utilChangeLogLevelCommand;
     private GameJoinCommand gameJoinCommand;
+
+    private AcceptTradeCommand acceptTradeCommand;
+    private BuildCityCommand buildCityCommand;
     private BuildRoadCommand buildRoadCommand;
+    private BuildSettlementCommand buildSettlementCommand;
+    private DiscardCommand discardCommand;
+    private EndTurnCommand endTurnCommand;
+    private MaritimeTradeCommand maritimeTradeCommand;
+    private OfferTradeCommand offerTradeCommand;
+    private PlayMonopolyCommand playMonopolyCommand;
+    private PlayMonumentCommand playMonumentCommand;
+    private PlayRoadBuilderCommand playRoadBuilderCommand;
+    private PlaySoldierCommand playSoldierCommand;
+    private PlayYearOfPlentyCommand playYearOfPlentyCommand;
+    private PurchaseDevCardCommand purchaseDevCardCommand;
+    private RobPlayerCommand robPlayerCommand;
+    private RollDiceCommand rollDiceCommand;
 
 
     public void setUp() throws Exception {
         super.setUp();
 
         setUpModelString();
+
+        //ADD AI CMD SETUP
+        //-----------------
+        //-----------------
 
         //GAME JOIN CMD SETUP
         //-----------------
@@ -42,19 +78,19 @@ public class JSONTranslatorTest extends TestCase {
         //-----------------
 
 
-
         //BUILD ROAD CMD SETUP
         //-----------------
         int bRPlayerID = 2;
         EdgeDirection bREdgeDir = EdgeDirection.NorthEast;
-        HexLocation brELHexLoc = new HexLocation(1,1);
+        HexLocation brELHexLoc = new HexLocation(1, 1);
         EdgeLocation bREdgeLocation = new EdgeLocation(brELHexLoc, bREdgeDir);
         buildRoadCommand = new BuildRoadCommand(bREdgeLocation, bRPlayerID);
         buildRoadCommand.setFree(true);
         //-----------------
 
     }
-    private void setUpModelString(){
+
+    private void setUpModelString() {
         // This is an actual server response body:
         testResponseBody = " {\n" +
                 "  \"deck\": {\n" +
@@ -376,9 +412,13 @@ public class JSONTranslatorTest extends TestCase {
                 "  },\n" +
                 "  \"winner\": -1,\n" +
                 "  \"version\": 0\n" +
-                "}" ;
+                "}";
     }
 
+
+    /**
+     * @throws Exception
+     */
     public void testModelTranslation() throws Exception {
         System.out.println(">I'M TESTING MODEL TRANSLATION!");
         //System.out.println("Test JSON string: \n");
@@ -401,7 +441,193 @@ public class JSONTranslatorTest extends TestCase {
 
     }
 
-    public void testBuildRoadCmdTranslation() throws Exception{
+//TEST GAME COMMANDS  ===============================
+
+    public void testAddAICmdTranslation() throws Exception {
+        System.out.println(">TESTING ADDAICMD TRANSLATION!");
+
+        String addAICmdJSONResult = gsonTest.toJson(gameJoinCommand);
+
+        System.out.println("Just serialized addAICmd, JSONstring result= ");
+        System.out.println(addAICmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testExecGameCmdsCmdTranslation() throws Exception {
+        System.out.println(">TESTING EXECGAMECMDSCMD TRANSLATION!");
+
+        String execGameCmdsCmdJSONResult = gsonTest.toJson(execGameCmdsCommand);
+
+        System.out.println("Just serialized execGameCmdsCmd, JSONstring result= ");
+        System.out.println(execGameCmdsCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testFetchNewModelCmdTranslation() throws Exception {
+        System.out.println(">TESTING FETCHNEWMODELCMD TRANSLATION!");
+
+        String fetchNewModelCmdJSONResult = gsonTest.toJson(fetchNewModelCommand);
+
+        System.out.println("Just serialized fetchNewModelCmd, JSONstring result= ");
+        System.out.println(fetchNewModelCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testGameCreateCmdTranslation() throws Exception {
+        System.out.println(">TESTING GAMECREATECMD TRANSLATION!");
+
+        String gameCreateCmdJSONResult = gsonTest.toJson(gameCreateCommand);
+
+        System.out.println("Just serialized gameCreateCmd, JSONstring result= ");
+        System.out.println(gameCreateCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testGameJoinCmdTranslation() throws Exception {
+        System.out.println(">TESTING GAMEJOINCMD TRANSLATION!");
+
+        String gameJoinCmdJSONResult = gsonTest.toJson(gameJoinCommand);
+
+        System.out.println("Just serialized gameJoinCmd, JSONstring result= ");
+        System.out.println(gameJoinCmdJSONResult);
+        System.out.println("=================");
+
+        //I don't know how picky we should be with these - our Color enum uses all caps, but the
+        //server swagger page wants lowercase. Also, we originally had "id" named "gameID", which
+        //I like better, but the swagger calls it "id" so... maybe for now just do exactly what it says?
+        String expectedResult =
+                " {\n" +
+                        "\"id\": \"5\"," +
+                        "\"color\": \"BLUE\"" +
+                        " }\n";
+
+        //     assertEquals(expectedResult, gameJoinCmdJSONResult);
+    }
+
+    public void testGameListCmdTranslation() throws Exception {
+        System.out.println(">TESTING GAMELISTCMD TRANSLATION!");
+
+        String gameListCmdJSONResult = gsonTest.toJson(gameListCommand);
+
+        System.out.println("Just serialized gameListCmd, JSONstring result= ");
+        System.out.println(gameListCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testGameLoadCmdTranslation() throws Exception {
+        System.out.println(">TESTING GAMELOADCMD TRANSLATION!");
+
+        String gameLoadCmdJSONResult = gsonTest.toJson(gameLoadCommand);
+
+        System.out.println("Just serialized gameLoadCmd, JSONstring result= ");
+        System.out.println(gameLoadCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testGameResetCmdTranslation() throws Exception {
+        System.out.println(">TESTING GAMERESETCMD TRANSLATION!");
+
+        String gameResetCmdJSONResult = gsonTest.toJson(gameResetCommand);
+
+        System.out.println("Just serialized gameResetCmd, JSONstring result= ");
+        System.out.println(gameResetCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testGameSaveCmdTranslation() throws Exception {
+        System.out.println(">TESTING GAMESAVECMD TRANSLATION!");
+
+        String gameSaveCmdJSONResult = gsonTest.toJson(gameSaveCommand);
+
+        System.out.println("Just serialized gameSaveCmd, JSONstring result= ");
+        System.out.println(gameSaveCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testGetGameCmdsCmdTranslation() throws Exception {
+        System.out.println(">TESTING GETCGAMECMDSCMD TRANSLATION!");
+
+        String getGameCmdsCmdJSONResult = gsonTest.toJson(getGameCmdsCommand);
+
+        System.out.println("Just serialized getGameCmdsCmd, JSONstring result= ");
+        System.out.println(getGameCmdsCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testListAICmdTranslation() throws Exception {
+        System.out.println(">TESTING LISTAICMD TRANSLATION!");
+
+        String listAICmdJSONResult = gsonTest.toJson(listAICommand);
+
+        System.out.println("Just serialized listAICmd, JSONstring result= ");
+        System.out.println(listAICmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testLoginCmdTranslation() throws Exception {
+        System.out.println(">TESTING LOGINCMD TRANSLATION!");
+
+        String loginCmdJSONResult = gsonTest.toJson(loginCommand);
+
+        System.out.println("Just serialized loginCmd, JSONstring result= ");
+        System.out.println(loginCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testRegisterCmdTranslation() throws Exception {
+        System.out.println(">TESTING REGISTERCMD TRANSLATION!");
+
+        String registerCmdJSONResult = gsonTest.toJson(registerCommand);
+
+        System.out.println("Just serialized registerCmd, JSONstring result= ");
+        System.out.println(registerCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testSendChatCmdTranslation() throws Exception {
+        System.out.println(">TESTING SENDCHATCMD TRANSLATION!");
+
+        String sendChatCmdJSONResult = gsonTest.toJson(sendChatCommand);
+
+        System.out.println("Just serialized sendChatCmd, JSONstring result= ");
+        System.out.println(sendChatCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testUtilChangeLogLevelCmdTranslation() throws Exception {
+        System.out.println(">TESTING UTILCHANGELOGLEVELCMD TRANSLATION!");
+
+        String utilChangeLogLevelCmdJSONResult = gsonTest.toJson(utilChangeLogLevelCommand);
+
+        System.out.println("Just serialized utilChangeLogLevelCmd, JSONstring result= ");
+        System.out.println(utilChangeLogLevelCmdJSONResult);
+        System.out.println("=================");
+    }
+
+
+//TEST MOVES COMMANDS  ===============================
+
+    public void testAcceptTradeCmdTranslation() throws Exception {
+        System.out.println(">TESTING ACCEPTTRADECMD TRANSLATION!");
+
+        String acceptTradeCmdJSONResult = gsonTest.toJson(acceptTradeCommand);
+
+        System.out.println("Just serialized acceptTradeCmd, JSONstring result= ");
+        System.out.println(acceptTradeCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testBuildCityCmdTranslation() throws Exception {
+        System.out.println(">TESTING BUILDCITYCMD TRANSLATION!");
+
+        String buildCityCmdJSONResult = gsonTest.toJson(buildCityCommand);
+
+        System.out.println("Just serialized buildCityCmd, JSONstring result= ");
+        System.out.println(buildCityCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testBuildRoadCmdTranslation() throws Exception {
         System.out.println(">TESTING BUILDROADCMD TRANSLATION!");
 
         String buildRoadCmdJSONResult = gsonTest.toJson(buildRoadCommand);
@@ -429,39 +655,155 @@ public class JSONTranslatorTest extends TestCase {
         the strings aren't exactly the same.
         According to SO, a JSON obj is an UNORDERED list of key/value pairs.
         Gson does not support field order specification.... unless you write your own custom serializer -_-
+
+        Options:
+         - build custom Gson parsers for every server command
+         - figure out Hamcrest
+         - download JSONassert and use that - super easy
+         - use Jackson to control field order
         */
 
         //****
         //Trying a workaround here, since building custom parsers for every command obj would suck:
-            //apparently Hamcrest is used just for this reason!
         //I think Hamcrest might be the solution to this, but I think I need to talk to the TAS
         // to figure out how to use it...
-        //org.hamcrest.MatcherAssert.assertThat
 
-       // assertEquals(expectedResult, buildRoadCmdJSONResult);
+        //MatcherAssert.assertThat(String expectedResult, T actual, org.hamcest.Matcher<? super T> matcher)
+
+        //this doesn't work
+        // assertEquals(expectedResult, buildRoadCmdJSONResult);
+        Assert.assertTrue(jsonExpectedResult.has("type"));
 
     }
 
-    public void testGameJoinCmdTranslation() throws Exception{
-        System.out.println(">TESTING GAMEJOINCMD TRANSLATION!");
+    public void testBuildSettlementCmdTranslation() throws Exception {
+        System.out.println(">TESTING BUILDSTLMTCMD TRANSLATION!");
 
-        String gameJoinCmdJSONResult = gsonTest.toJson(gameJoinCommand);
+        String buildStlmtCmdJSONResult = gsonTest.toJson(buildSettlementCommand);
 
-        System.out.println("Just serialized gameJoinCmd, JSONstring result= ");
-        System.out.println(gameJoinCmdJSONResult);
+        System.out.println("Just serialized buildStlmtCmd, JSONstring result= ");
+        System.out.println(buildStlmtCmdJSONResult);
         System.out.println("=================");
+    }
 
+    public void testDiscardCmdTranslation() throws Exception {
+        System.out.println(">TESTING DISCARDCMD TRANSLATION!");
 
-        //I don't know how picky we should be with these - our Color enum uses all caps, but the
-        //server swagger page wants lowercase. Also, we originally had "id" named "gameID", which
-        //I like better, but the swagger calls it "id" so... maybe for now just do exactly what it says?
-        String expectedResult =
-                        " {\n" +
-                        "\"id\": \"5\"," +
-                        "\"color\": \"BLUE\"" +
-                        " }\n";
+        String discardCmdJSONResult = gsonTest.toJson(discardCommand);
 
-   //     assertEquals(expectedResult, gameJoinCmdJSONResult);
+        System.out.println("Just serialized discardCmd, JSONstring result= ");
+        System.out.println(discardCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testEndTurnCmdTranslation() throws Exception {
+        System.out.println(">TESTING ENDTURNCMD TRANSLATION!");
+
+        String endTurnCmdJSONResult = gsonTest.toJson(endTurnCommand);
+
+        System.out.println("Just serialized endTurnCmd, JSONstring result= ");
+        System.out.println(endTurnCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testMaritimeTradeCmdTranslation() throws Exception {
+        System.out.println(">TESTING MARITIMETRADECMD TRANSLATION!");
+
+        String maritimeTradeCmdJSONResult = gsonTest.toJson(maritimeTradeCommand);
+
+        System.out.println("Just serialized maritimeTradeCMD, JSONstring result= ");
+        System.out.println(maritimeTradeCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testOfferTradeCmdTranslation() throws Exception {
+        System.out.println(">TESTING OFFERTRADECMD TRANSLATION!");
+
+        String offerTradeCmdJSONResult = gsonTest.toJson(offerTradeCommand);
+
+        System.out.println("Just serialized offerTradeCMD, JSONstring result= ");
+        System.out.println(offerTradeCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testPlayMonopolyCmdTranslation() throws Exception {
+        System.out.println(">TESTING PLAYMONOPOLYCMD TRANSLATION!");
+
+        String playMonopolyCmdJSONResult = gsonTest.toJson(playMonopolyCommand);
+
+        System.out.println("Just serialized playMonopolyCmd, JSONstring result= ");
+        System.out.println(playMonopolyCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testPlayMonumentCmdTranslation() throws Exception {
+        System.out.println(">TESTING PLAYMONUMENTCMD TRANSLATION!");
+
+        String playMonumentCmdJSONResult = gsonTest.toJson(playMonumentCommand);
+
+        System.out.println("Just serialized playMonumentCmd, JSONstring result= ");
+        System.out.println(playMonumentCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testPlayRoadBuilderCmdTranslation() throws Exception {
+        System.out.println(">TESTING PLAYROADBUILDERCMD TRANSLATION!");
+
+        String playRoadBuilderCmdJSONResult = gsonTest.toJson(playRoadBuilderCommand);
+
+        System.out.println("Just serialized playRoadBuilderCmd, JSONstring result= ");
+        System.out.println(playRoadBuilderCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testPlaySoldierCmdTranslation() throws Exception {
+        System.out.println(">TESTING PLAYSOLDIERCMD TRANSLATION!");
+
+        String playSoldierCmdJSONResult = gsonTest.toJson(playSoldierCommand);
+
+        System.out.println("Just serialized playSoldierCmd, JSONstring result= ");
+        System.out.println(playSoldierCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testPlayYearOfPlentyCmdTranslation() throws Exception {
+        System.out.println(">TESTING PLAYYEAROFPLENTYCMD TRANSLATION!");
+
+        String playYearOfPlentyCmdJSONResult = gsonTest.toJson(playYearOfPlentyCommand);
+
+        System.out.println("Just serialized playYearOfPlentyCmd, JSONstring result= ");
+        System.out.println(playYearOfPlentyCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testPurchaseDevCardCmdTranslation() throws Exception {
+        System.out.println(">TESTING PURCHASEDEVDARDCMD TRANSLATION!");
+
+        String purchaseDevCardCmdJSONResult = gsonTest.toJson(purchaseDevCardCommand);
+
+        System.out.println("Just serialized playYearOfPlentyCmd, JSONstring result= ");
+        System.out.println(purchaseDevCardCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testRobPlayerCmdTranslation() throws Exception {
+        System.out.println(">TESTING ROBPLAYERCMD TRANSLATION!");
+
+        String robPlayerCmdJSONResult = gsonTest.toJson(robPlayerCommand);
+
+        System.out.println("Just serialized robPlayerCmd, JSONstring result= ");
+        System.out.println(robPlayerCmdJSONResult);
+        System.out.println("=================");
+    }
+
+    public void testRollDiceCmdTranslation() throws Exception {
+        System.out.println(">TESTING ROLLDICECMD TRANSLATION!");
+
+        String rollDiceCmdJSONResult = gsonTest.toJson(rollDiceCommand);
+
+        System.out.println("Just serialized rollDiceCmd, JSONstring result= ");
+        System.out.println(rollDiceCmdJSONResult);
+        System.out.println("=================");
     }
 
 }
