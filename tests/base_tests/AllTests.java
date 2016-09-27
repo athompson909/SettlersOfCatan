ee8a62718531c29b658b6c078a433738f19adf7f
athompson909 <athompson909@gmail.com>
2016-09-27 16:55:33 -0600

    added AllTests and build.xml and some updates

--- tests/base_tests/AllTests.java
+++ tests/base_tests/AllTests.java
@@ -1,14 +1,15 @@
-package tests;
+package base_tests;
 
 /**
  * Created by adamthompson on 9/27/16.
  */
 public class AllTests {
 
+
     public static void main(String[] args) {
 
 
-        String[] testClasses = new String[] {"client_tests.ServerProxyTest"};
+        String[] testClasses = new String[] {"client_tests.ServerProxyTest"};//add the rest of the junits
 
         org.junit.runner.JUnitCore.main(testClasses);
     }
