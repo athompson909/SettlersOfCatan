6a2e4e5225bf068079e7a088d26581495c76c1af
wiseman5 <wiseman5@studentbody.byu.edu>
2016-09-27 19:18:46 -0600

    Poller test

---
+++ tests/client_tests/ServerPollerTest.java
@@ -0,0 +1,26 @@
+package client_tests;
+
+import client.ClientFacade;
+import client.IServerProxy;
+import client.MockProxy;
+import client.ServerPoller;
+import junit.framework.TestCase;
+import shared.model.ClientModel;
+
+import java.util.Timer;
+
+/**
+ * Created by Alise on 9/27/2016.
+ */
+public class ServerPollerTest extends TestCase {
+    private IServerProxy proxy = new MockProxy();
+    private ClientModel model = new ClientModel(0);
+    private ClientFacade clientFacade = new ClientFacade(proxy, model);
+    private ServerPoller poller = new ServerPoller(clientFacade);
+
+    private Timer timer = new Timer();
+
+    public void pollerTest() {
+        
+    }
+}
