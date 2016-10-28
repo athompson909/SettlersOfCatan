901eb0e2c9df5b88f1835e356a238cf4eeb756da
athompson909 <athompson909@gmail.com>
2016-10-28 12:28:06 -0600

    host port setter

--- src/client/main/Catan.java
+++ src/client/main/Catan.java
@@ -68,10 +68,7 @@ public class Catan extends JFrame
 				System.out.println("CATAN ************ args length =" + args.length);
 
 				//if they specified a host/port number
-				if (args.length == 0){
-					//use default host/port number (localhost:8081)
-				}
-				else if (args.length == 1){
+				if (args.length == 1) {
 				  //error
 					System.out.println("ERROR: invalid host/port args");
 					System.exit(0);
