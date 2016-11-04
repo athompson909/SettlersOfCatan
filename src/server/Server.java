4d1584194026a76661c04052d77a557110799605
athompson909 <athompson909@gmail.com>
2016-11-04 12:53:14 -0600

    server functionality

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,7 +1,54 @@
 package server;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.lang.String;
+
 /**
+ *
+ *
+ *
  * Created by adamthompson on 11/4/16.
  */
 public class Server {
+
+    private HttpServer httpServer;
+
+    private String hostNumber;
+
+    private int portNumber;
+
+    private final int MAX_WAITING_CONNECTIONS = 20;
+
+    public Server(String hostNumber, int portNumber) {
+        setHostNumber(hostNumber);
+        setPortNumber(portNumber);
+    }
+
+    public void run() {
+        try {
+            httpServer = HttpServer.create(new InetSocketAddress(portNumber), MAX_WAITING_CONNECTIONS);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getHostNumber() {
+        return hostNumber;
+    }
+
+    public void setHostNumber(String hostNumber) {
+        this.hostNumber = hostNumber;
+    }
+
+    public int getPortNumber() {
+        return portNumber;
+    }
+
+    public void setPortNumber(int portNumber) {
+        this.portNumber = portNumber;
+    }
 }
