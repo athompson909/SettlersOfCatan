8aa626219794dd4172ea9ba6cf7713946639db05
SierraD012 <sd.birdlover@gmail.com>
2016-11-05 12:40:46 -0600

    Added ServerTranslatorTest class

---
+++ tests/server_tests/ServerTranslatorTest.java
@@ -0,0 +1,33 @@
+package server_tests;
+
+import junit.framework.TestCase;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+/**
+ * This is the JUnit test class for the ServerTranslator.
+ *
+ * Created by Sierra on 11/05/16.
+ */
+public class ServerTranslatorTest extends TestCase {
+
+
+    @Test
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    /*
+    @Test
+    public void testAddAICmdTranslation() throws Exception {
+        System.out.println(">TESTING ADDAICMD TRANSLATION!");
+
+
+       // JSONAssert.assertEquals(expectedResult, addAICmdJSONResult, JSONCompareMode.NON_EXTENSIBLE);
+    }
+    */
+
+}
