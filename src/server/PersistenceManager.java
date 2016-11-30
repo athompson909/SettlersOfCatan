63d985c5d807a7aefc483c5a01130c36e025cdc0
wiseman5 <wiseman5@studentbody.byu.edu>
2016-11-30 12:51:15 -0700

    command manager(reaches limit) and persistenceManager (singleton)

--- src/server/PersistenceManager.java
+++ src/server/PersistenceManager.java
@@ -1,7 +1,28 @@
 package server;
 
+import shared.model.commandmanager.BaseCommand;
+
 /**
  * Created by adamthompson on 11/29/16.
  */
 public class PersistenceManager {
+    private static PersistenceManager instance = new PersistenceManager();
+    public static PersistenceManager getInstance(){
+        return instance;
+    }
+    private PersistenceManager() {
+
+    }
+
+    public void writeGame(int gameID){
+
+    }
+
+    public void clearCommands(int gameID){
+
+    }
+
+    public void writeCommand(int gameID, BaseCommand command){
+
+    }
 }
